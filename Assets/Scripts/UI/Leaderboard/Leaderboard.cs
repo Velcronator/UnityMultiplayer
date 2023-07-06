47a85734ef9e9bb2a728e549b596a6c8f077f36c
velcronator <valacos7@gmail.com>
2023-07-06 23:44:32 +1000

    Leaderboard Spawning:

--- Assets/Scripts/UI/Leaderboard/Leaderboard.cs
+++ Assets/Scripts/UI/Leaderboard/Leaderboard.cs
@@ -1,18 +1,96 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
-public class Leaderboard : MonoBehaviour
+public class Leaderboard : NetworkBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Transform leaderboardEntityHolder;
+    [SerializeField] private LeaderboardEntityDisplay leaderboardEntityPrefab;
+
+    private NetworkList<LeaderboardEntityState> leaderboardEntities;
+
+    private void Awake()
     {
-        
+        leaderboardEntities = new NetworkList<LeaderboardEntityState>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void OnNetworkSpawn()
     {
-        
+        if (IsClient)
+        {
+            leaderboardEntities.OnListChanged += HandleLeaderboardEntitiesChanged;
+            foreach (LeaderboardEntityState entity in leaderboardEntities)
+            {
+                HandleLeaderboardEntitiesChanged(new NetworkListEvent<LeaderboardEntityState>
+                {
+                    Type = NetworkListEvent<LeaderboardEntityState>.EventType.Add,
+                    Value = entity
+                });
+            }
+        }
+
+        if (IsServer)
+        {
+            TankPlayer[] players = FindObjectsByType<TankPlayer>(FindObjectsSortMode.None);
+            foreach (TankPlayer player in players)
+            {
+                HandlePlayerSpawned(player);
+            }
+
+            TankPlayer.OnPlayerSpawned += HandlePlayerSpawned;
+            TankPlayer.OnPlayerDespawned += HandlePlayerDespawned;
+        }
+    }
+
+    public override void OnNetworkDespawn()
+    {
+        if (IsClient)
+        {
+            leaderboardEntities.OnListChanged -= HandleLeaderboardEntitiesChanged;
+        }
+
+        if (IsServer)
+        {
+            TankPlayer.OnPlayerSpawned -= HandlePlayerSpawned;
+            TankPlayer.OnPlayerDespawned -= HandlePlayerDespawned;
+        }
+    }
+
+    private void HandleLeaderboardEntitiesChanged(NetworkListEvent<LeaderboardEntityState> changeEvent)
+    {
+        switch (changeEvent.Type)
+        {
+            case NetworkListEvent<LeaderboardEntityState>.EventType.Add:
+                Instantiate(leaderboardEntityPrefab, leaderboardEntityHolder);
+                break;
+            case NetworkListEvent<LeaderboardEntityState>.EventType.Remove:
+                break;
+        }
+    }
+
+    private void HandlePlayerSpawned(TankPlayer player)
+    {
+        leaderboardEntities.Add(new LeaderboardEntityState
+        {
+            ClientId = player.OwnerClientId,
+            PlayerName = player.PlayerName.Value,
+            Coins = 0
+        });
+    }
+
+    private void HandlePlayerDespawned(TankPlayer player)
+    {
+        // Only in newer version of Netcode
+        if(leaderboardEntities == null) { return; }
+
+        foreach (LeaderboardEntityState entity in leaderboardEntities)
+        {
+            if (entity.ClientId != player.OwnerClientId) { continue; }
+
+            leaderboardEntities.Remove(entity);
+            break;
+        }
     }
 }
