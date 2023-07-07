87971264244a9692e1d6209bf57b64addd6605e8
velcronator <valacos7@gmail.com>
2023-07-07 21:09:13 +1000

    Restoring Health

--- Assets/Scripts/Core/Combat/HealingZone.cs
+++ Assets/Scripts/Core/Combat/HealingZone.cs
@@ -16,7 +16,33 @@ public class HealingZone : NetworkBehaviour
     [SerializeField] private int coinsPerTick = 10;
     [SerializeField] private int healthPerTick = 10;
 
+    private float remainingCooldown;
+    private float tickTimer;
     private List<TankPlayer> playersInZone = new List<TankPlayer>();
+
+    private NetworkVariable<int> HealPower = new NetworkVariable<int>();
+
+    public override void OnNetworkSpawn()
+    {
+        if (IsClient)
+        {
+            HealPower.OnValueChanged += HandleHealPowerChanged;
+            HandleHealPowerChanged(0, HealPower.Value);
+        }
+
+        if (IsServer)
+        {
+            HealPower.Value = maxHealPower;
+        }
+    }
+
+    public override void OnNetworkDespawn()
+    {
+        if (IsClient)
+        {
+            HealPower.OnValueChanged -= HandleHealPowerChanged;
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D col)
     {
@@ -25,8 +51,6 @@ public class HealingZone : NetworkBehaviour
         if (!col.attachedRigidbody.TryGetComponent<TankPlayer>(out TankPlayer player)) { return; }
 
         playersInZone.Add(player);
-
-        Debug.Log($"Entered: {player.PlayerName.Value}");
     }
 
     private void OnTriggerExit2D(Collider2D col)
@@ -36,7 +60,54 @@ public class HealingZone : NetworkBehaviour
         if (!col.attachedRigidbody.TryGetComponent<TankPlayer>(out TankPlayer player)) { return; }
 
         playersInZone.Remove(player);
+    }
 
-        Debug.Log($"Left: {player.PlayerName.Value}");
+    private void Update()
+    {
+        if (!IsServer) { return; }
+
+        if (remainingCooldown > 0f)
+        {
+            remainingCooldown -= Time.deltaTime;
+
+            if (remainingCooldown <= 0f)
+            {
+                HealPower.Value = maxHealPower;
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        tickTimer += Time.deltaTime;
+        if (tickTimer >= 1 / healTickRate)
+        {
+            foreach (TankPlayer player in playersInZone)
+            {
+                if (HealPower.Value == 0) { break; }
+
+                if (player.Health.CurrentHealth.Value == player.Health.MaxHealth) { continue; }
+
+                if (player.Wallet.TotalCoins.Value < coinsPerTick) { continue; }
+
+                player.Wallet.SpendCoins(coinsPerTick);
+                player.Health.RestoreHealth(healthPerTick);
+
+                HealPower.Value -= 1;
+
+                if (HealPower.Value == 0)
+                {
+                    remainingCooldown = healCooldown;
+                }
+            }
+
+            tickTimer = tickTimer % (1 / healTickRate);
+        }
+    }
+
+    private void HandleHealPowerChanged(int oldHealPower, int newHealPower)
+    {
+        healPowerBar.fillAmount = (float)newHealPower / maxHealPower;
     }
 }
