a3147d221642ffb4aeed1eb4ab338372f438bcf2
velcronator <valacos7@gmail.com>
2023-07-11 01:10:30 +1000

    Matchmaking UI

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -5,7 +5,20 @@ using UnityEngine;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] private TMP_Text queueStatusText;
+    [SerializeField] private TMP_Text queueTimerText;
+    [SerializeField] private TMP_Text findMatchButtonText;
     [SerializeField] private TMP_InputField joinCodeField;
+
+    private void Start()
+    {
+        if (ClientSingleton.Instance == null) { return; }
+
+        Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
+
+        queueStatusText.text = string.Empty;
+        queueTimerText.text = string.Empty;
+    }
 
     public async void StartHost()
     {
